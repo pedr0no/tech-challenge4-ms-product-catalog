f3ee56bc49557c08ac073edf1cb1ae7f93340018
Pedro Ono <pedroono@icloud.com>
2024-11-03 21:15:42 -0300

    Criando estrutura do MS

---
+++ src/main/java/br/com/tech/challenge/product_catalog_management/ProductCatalogManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech.challenge.product_catalog_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tech/challenge/product_catalog_management/ProductCatalogManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech.challenge.product_catalog_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
